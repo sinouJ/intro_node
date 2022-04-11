67dd499f146efe466325e914eada506ec9205091
Jordan Sinou <sinoujordan@gmail.com>
2022-04-11 14:56:36 +0200

    correction

--- controllers/thingController.js
+++ controllers/thingController.js
@@ -1,11 +1,30 @@
 module.exports = {
-    thing: (req, res) => {
+    user: (req, res) => {
         var user = 'Jordan'
         res.status(200).render('index', {
             user
         })
     }, 
-    index: (req, res) => {
-        res.send('Hello from the index!');
+    users: (req, res) => {
+        var users = [
+            {
+                firstname: 'Jordan',
+                name: 'Sinou',
+                age: '24'
+            },
+            {
+                firstname: 'jo',
+                name: 'si',
+                age: '14'
+            },
+            {
+                firstname: 'john',
+                name: 'doe',
+                age: '34'
+            }
+        ]
+        res.status(200).render('users', {
+            users
+        })
     }
 }
--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,8 @@ const thingController = require('../controllers/thingController');
 exports.router = (function() {
     const router = express.Router()
 
-    router.route('/').get(thingController.thing)
+    // router.route('/').get(thingController.thing)
+    router.route('/users').get(thingController.users)
     // router.route('/').get(thingController.index)
 
     return router
